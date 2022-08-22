f3c3482d9a7b56e449ff4c2f9e911e4d0442b197
Alex Correia <dev.alexcorreia@gmail.com>
2022-08-22 10:16:54 -0300

    att 0.1.1

--- Animal Combat/Animal.java
+++ Animal Combat/Animal.java
@@ -1,58 +1,85 @@
+import java.util.Random;
 public class Animal {
-    
-  private Boolean vivo;
-  private int massa;
+  
+  Random random = new Random();
+  private Boolean alive;
   private int x;// x-axis motion indicator
   private int y;//y-axis motion indicator
-  private int vel;
-  private int forca;
-  private int vida;
-    public Animal(int vel, int massa, int forca){
-        this.vivo=true;
-        this.massa=massa;
+  private int strenght;
+  private int velocity;
+  private int resistence=100; 
+  private int life=100;
+  private int stamina=100;
+  private int damage;
+
+    public Animal(int velocity, int mass, int strenght){
+        this.alive=true;
         this.x=0;
         this.y=0;
-        this.vel=vel;
-        this.forca=forca;
-        this.vida=100;
-    }
-    public int getMassa(){
-        return this.massa;
-    }
-    public void setMassa(int massa){
-        this.massa=massa;
-    }
-    public int getForca(){
-        return this.forca;
-    }
-    public void setForca(int forca){
-        this.forca=forca;
-    }
-    public int getVel(){
-        return this.vel;
-    }
-    public void setvel(int vel){
-        this.vel=vel;
+        this.strenght=strenght;
+        this.velocity=velocity;
+        this.resistence=resistence;
+        this.life=100;
+        this.stamina=stamina;
+        this.damage=damage;
     }
     
-    public void atacar(){
+    public int getstrenght(){
+        return this.strenght;
+    }
+    public void setStrenght(int strenght){
+        this.strenght=strenght;
+    }
+    public int getVelocity(){
+        return this.velocity;
+    }
+    public void setVelocity(int velocity){
+        this.velocity=velocity;
+    }
+    
+    public void attack(Animal Tiger){
+       do{
+        if(this.stamina>0){            
         
-    }
-    public void mover(){
-        if(this.vivo){
-            this.x+=this.vel;
-            this.y+=this.vel;
-
+        if(this.velocity>Tiger.velocity){
+            this.strenght+=5;
+    }else{
+            Tiger.resistence+=10;
         }
+        if(this.strenght>Tiger.resistence){
+            Tiger.resistence-=10;
+            this.damage=random.nextInt(40);
+            Tiger.life-=this.damage;
+            this.stamina-=5;
+            Tiger.stamina-=20;
+            if(Tiger.life==0){
+                Tiger.alive=false;
+                System.out.println("Tiger was fainted");
+
+            }
+        }
+    }else{
+        Tiger.resistence+=10;
+            this.damage=random.nextInt(15);
+            Tiger.life-=this.damage;
+            this.stamina-=15;
+            Tiger.stamina-=15;
+            if(Tiger.life==0){
+                Tiger.alive=false;
+                System.out.println("Tiger was fainted");
     }
-    public void comer(int massa){}
+}
+       
+}while(this.stamina==0 || Tiger.stamina==0);
+    }   
+    public void comer(int mass){}
     public void info(){
         System.out.print("-------------------------------------");
-        System.out.printf("A Vida do animal é: %d",this.vida);
-        System.out.printf("A Velocidade do animal é: %d",this.vel);
-        System.out.printf("A Força do animal é: %d",this.forca);
+        System.out.printf("The Animals life is now: %d",this.life);
+        System.out.printf("A Velocidade do animal é: %d",this.velocity);
+        System.out.printf("A Força do animal é: %d",this.strenght);
 
     }
     
-
+    
 }
---
+++ Animal Combat/Tiger.java
@@ -0,0 +1,8 @@
+public class Tiger extends Animal {
+    
+    public Tiger(){
+        super(65, 45, 89);
+ 
+ }
+ 
+}
