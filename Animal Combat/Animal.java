2ba9652cf242460ed1e85dffb2be24fb690f0d7d
Alex Correia <dev.alexcorreia@gmail.com>
2022-08-22 11:34:48 -0300

    att 0.0.3

--- Animal Combat/Animal.java
+++ Animal Combat/Animal.java
@@ -2,19 +2,19 @@ import java.util.Random;
 public class Animal {
   
   Random random = new Random();
-  private Boolean alive;
+  private Boolean alive=true;
   //private int x;// x-axis motion indicator
   //private int y;//y-axis motion indicator
-  private int strenght;
+  private int strenght=100;
   private int velocity;
-  private int resistence=100; 
+  private int resistence=50; 
   private int life=100;
   private int stamina=100;
   private int damage;
 
     public Animal(int velocity, int stamina, int strenght){
     }   
-    
+    //______________________________________SET's and GET's_______________________________________________________
     public int getstrenght(){
         return this.strenght;
     }
@@ -51,11 +51,12 @@ public class Animal {
     public void setDamage(int damage){
         this.damage=damage;
     }
+    //____________________________________________ATTACK FUNCTION______________________________________________________
     public void attack(Animal Tiger){
-       do{
-        if(this.stamina>0){            
+       
+        if(this.stamina>0){ // if stamina is higher then 0 so you can go foward           
         
-        if(this.velocity>Tiger.velocity){
+        if(this.velocity>Tiger.velocity){ // if your velocity is higher then your enemy's so you can go foward
             this.strenght+=5;
     }else{
             Tiger.resistence+=10;
@@ -72,7 +73,7 @@ public class Animal {
 
             }
         }
-    }else{
+    }else{// 
         Tiger.resistence+=10;
             this.damage=random.nextInt(15);
             Tiger.life-=this.damage;
@@ -83,16 +84,14 @@ public class Animal {
                 System.out.println("Tiger was fainted");
     }
 }
-       
-}while(this.stamina==0 || Tiger.stamina==0);
+   
     }   
-    public void comer(int mass){}
+//_________________________________________INFORMATION FUNCTION___________________________________________________  
     public void info(){
         System.out.print("-------------------------------------");
-        System.out.printf("The Animals life is now: %d\n",this.alive);
-        System.out.printf("A Velocidade do animal é: %d\n",this.stamina);
-        System.out.printf("A Força do animal é: %d\n",this.life);
-
+        System.out.printf("Animals Stamina is now: %d\n",this.stamina);
+        System.out.printf("Animals Life is: %d\n",this.life);
+        System.out.printf("Animals resistence is: %d\n",this.resistence);
     }
     
     
--- Animal Combat/Tela.java
+++ Animal Combat/Tela.java
@@ -1,9 +1,10 @@
 public class Tela {
     public static void main (String[]args){
-        Animal Tiger = new Animal(80,30,86);
-        Animal Lion = new Animal(86,30,89);
+        Animal tiger = new Animal(89,30,86);
+        Animal lion = new Animal(86,30,89);
 
-        
+        tiger.attack(lion);
+        tiger.info();
         
     }
 }
